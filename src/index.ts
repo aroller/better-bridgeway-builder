a4a22b71946e55920772c38f8e930f50ba7ca73a
Aaron Roller <aaron.roller@aawhere.com>
2023-10-03 18:11:46 +0000

    refactor index to use scene

--- src/index.ts
+++ src/index.ts
@@ -1,86 +1,110 @@
 import { Street, Lane, LaneDirection } from "./street";
 import { Player } from "./player";
 
-function init(ctx: CanvasRenderingContext2D) {
-    const canvas = ctx.canvas;
+/**
+ * Represents a game scene with a canvas, street, and player.
+ */
+/**
+ * The Scene class manages the rendering of the canvas for the game, as well as the street and player objects.
+ */
+class Scene {
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+    private street: Street;
+    private player: Player;
+    private topOfStreetY: number;
 
-    const streetLength = canvas.width;
-    const vehicleLaneWidth = 120;
-    const bikeLaneWidth = 50;
-    const topOfStreetY = canvas.height / 10; //use a buffer at the top 
-    let street = new Street()
-        .addLane(new Lane(LaneDirection.LEFT, bikeLaneWidth, streetLength))
-        .addLane(new Lane(LaneDirection.LEFT, vehicleLaneWidth, streetLength))
-        .addLane(new Lane(LaneDirection.RIGHT, vehicleLaneWidth, streetLength))
-        .addLane(new Lane(LaneDirection.RIGHT, bikeLaneWidth, streetLength))
-        .generateObstacles(); // Add obstacles to the lanes
+    /**
+     * Creates a new Scene instance.
+     * @param canvas - The HTMLCanvasElement to render the game on.
+     * @param ctx - The CanvasRenderingContext2D to use for rendering.
+     */
+    constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.topOfStreetY = canvas.height / 10;
 
-    const playerSize = 20;
-    let player = new Player(
-        streetLength / 2,
-        street.getStreetWidth() + playerSize + topOfStreetY, // add topOfStreetY to the starting position of the player
-        playerSize,
-        playerSize,
-    );
+        const streetLength = canvas.width;
+        const vehicleLaneWidth = 120;
+        const bikeLaneWidth = 50;
 
-    // Event listener for keyboard input
-    document.addEventListener("keydown", function (event) {
+        // Create the street object with four lanes, two for vehicles and two for bikes.
+        this.street = new Street()
+            .addLane(new Lane(LaneDirection.LEFT, bikeLaneWidth, streetLength))
+            .addLane(new Lane(LaneDirection.LEFT, vehicleLaneWidth, streetLength))
+            .addLane(new Lane(LaneDirection.RIGHT, vehicleLaneWidth, streetLength))
+            .addLane(new Lane(LaneDirection.RIGHT, bikeLaneWidth, streetLength))
+            .generateObstacles();
+
+        // Create the player object in the middle of the street.
+        const playerSize = 20;
+        this.player = new Player(
+            streetLength / 2,
+            this.street.getStreetWidth() + playerSize + this.topOfStreetY,
+            playerSize,
+            playerSize,
+        );
+
+        // Listen for keyboard input to move the player.
+        document.addEventListener("keydown", this.handleKeyDown.bind(this));
+
+        // Update the game every 50 milliseconds.
+        setInterval(() => {
+            this.street = this.street.updateObstacles();
+            this.updateCanvas();
+        }, 50);
+    }
+
+    /**
+     * Handles keyboard input to move the player.
+     * @param event - The KeyboardEvent object representing the key press.
+     */
+    private handleKeyDown(event: KeyboardEvent) {
         switch (event.code) {
             case "ArrowUp":
-                player = player.moveUp();
+                this.player = this.player.moveUp();
                 break;
             case "ArrowDown":
-                player = player.moveDown();
+                this.player = this.player.moveDown();
                 break;
             case "ArrowLeft":
-                player = player.moveLeft();
+                this.player = this.player.moveLeft();
                 break;
             case "ArrowRight":
-                player = player.moveRight();
+                this.player = this.player.moveRight();
                 break;
         }
 
-        // Update the canvas
-        updateCanvas(ctx, player, street, topOfStreetY);
-    });
+        this.updateCanvas();
+    }
 
-    // Initial canvas update and obstacle generation
-    let updatedStreet = street;
-    setInterval(() => {
-        updatedStreet = updatedStreet.updateObstacles();
-        updateCanvas(ctx, player, updatedStreet, topOfStreetY);
-    }, 50);
+    /**
+     * Updates the canvas with the current state of the game.
+     */
+    private updateCanvas() {
+        // Clear the canvas.
+        this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+        // Update the street obstacles.
+        this.street = this.street.updateObstacles();
+
+        // Draw the player and street.
+        this.player.draw(this.ctx);
+        this.street.draw(this.ctx, this.topOfStreetY);
+    }
 }
 
-function updateCanvas(
-    ctx: CanvasRenderingContext2D,
-    player: Player,
-    street: Street,
-    topOfStreetY: number,
-) {
-    // clear the canvas
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-
-    // Update the obstacles in each lane
-    street = street.updateObstacles();
-
-    // Draw the scene
-    player.draw(ctx);
-    street.draw(ctx, topOfStreetY);
-}
-
-// Get the canvas element and context
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 
 if (canvas) {
     const ctx = canvas.getContext("2d");
 
     if (ctx) {
-        // Initialize canvas and obstacles
-        init(ctx);
+        new Scene(canvas, ctx);
     } else {
         console.error("Canvas context is null");
     }
 } else {
     console.error("Canvas element is null");
 }
+
